283679b0d4580c1ee4179aef353f217c1b5b7df3
Alvaro Lopez de Ocariz <a.lopezdeocarizde@opendeusto.es>
2025-10-29 20:20:56 +0100

    Merge branch 'master' of https://github.com/AlvaroLopezdeOcariz/DeustoClubSports.git
    Continuacion de la ventana principal

--- src/Swing/VentanaInstalaciones.java
+++ src/Swing/VentanaInstalaciones.java
@@ -4,8 +4,12 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class VentanaInstalaciones extends JFrame{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	public VentanaInstalaciones() {
-		
+		setBounds(400, 200, 800, 500);
 		
 		
 		
--- src/Swing/VentanaPrincipal.java
+++ src/Swing/VentanaPrincipal.java
@@ -1,7 +1,9 @@
 package Swing;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
@@ -9,30 +11,45 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
 public class VentanaPrincipal extends JFrame{
 	
 	private static final long serialVersionUID = 1L;
 	private JPanel pNorte, pCentro, pSur;
-	private JButton btnCafeteria, btnTienda, btnInstalaciones, btnVolver;
+	private JButton btnCafeteria, btnTienda, btnInstalaciones;
 	private JLabel lblImagen1, lblImagen2, lblImagen3;
 	public VentanaPrincipal() {
 		
 		//Propiedades de la Ventana
 		setBounds(400, 200, 800, 500);
+		setTitle("DeustoClubSports");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		
-		//Creación de componentes
-		pNorte = new JPanel(new BorderLayout(150, 30)); 
+		// Panel Norte 
+		pNorte = new JPanel(new BorderLayout(150, 30));
+		pNorte.setBorder(new EmptyBorder(10,10,10,10));
+		
+		btnCafeteria = new JButton("CAFETERÍA");
+		btnCafeteria.setFocusPainted(false);
+		btnCafeteria.setBackground(new Color(230, 230, 230));
+		btnCafeteria.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		
+		btnInstalaciones = new JButton("INSTALACIONES");
+		btnInstalaciones.setBounds(10, 10, 100, 30);
+		btnInstalaciones.setFocusPainted(false);
+		btnInstalaciones.setBackground(new Color(230, 230, 230));
+		btnInstalaciones.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		
+		btnTienda = new JButton("TIENDA");
+		btnTienda.setFocusPainted(false);
+		btnTienda.setBackground(new Color(230, 230, 230));
+		btnTienda.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		
+		// Panel Centro
 		pCentro = new JPanel();
-		pSur = new JPanel();
-		
-		btnCafeteria = new JButton("Cafetería");
-		btnInstalaciones = new JButton("Instalaciones");
-		btnInstalaciones.setBounds(4,4,4,4);
-		btnTienda = new JButton("Tienda");
-		btnVolver = new JButton("Volver");
-		
 		
         lblImagen1 = new JLabel();
         lblImagen2 = new JLabel();
@@ -40,20 +57,23 @@ public class VentanaPrincipal extends JFrame{
         
        
         ImageIcon imgOriginal1 = new ImageIcon("img/img1.jpg");
-        Image imgEscalada1 = imgOriginal1.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+        Image imgEscalada1 = imgOriginal1.getImage().getScaledInstance(250, 400, Image.SCALE_SMOOTH);
         ImageIcon img1 = new ImageIcon(imgEscalada1);
         
         ImageIcon imgOriginal2 = new ImageIcon("img/img2.jpg");
-        Image imgEscalada2 = imgOriginal2.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+        Image imgEscalada2 = imgOriginal2.getImage().getScaledInstance(250, 400, Image.SCALE_SMOOTH);
         ImageIcon img2 = new ImageIcon(imgEscalada2);
         
         ImageIcon imgOriginal3 = new ImageIcon("img/img3.jpg");
-        Image imgEscalada3 = imgOriginal3.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+        Image imgEscalada3 = imgOriginal3.getImage().getScaledInstance(250, 400, Image.SCALE_SMOOTH);
         ImageIcon img3 = new ImageIcon(imgEscalada3);
         
         lblImagen1.setIcon(img1);
         lblImagen2.setIcon(img2);
         lblImagen3.setIcon(img3);
+        
+        // Panel Sur
+        pSur = new JPanel();
 
 		//Añadir 
 		pNorte.add(btnCafeteria, BorderLayout.WEST);       
@@ -67,7 +87,6 @@ public class VentanaPrincipal extends JFrame{
 		pCentro.add(lblImagen1);
 		pCentro.add(lblImagen2);
 		pCentro.add(lblImagen3);
-		pSur.add(btnVolver);
 		
 		
 		//LISTENERS
