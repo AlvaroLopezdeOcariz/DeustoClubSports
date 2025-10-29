2ec0a80171f55a31e858c44f4074df9ae37b5166
Alvaro Lopez de Ocariz <a.lopezdeocarizde@opendeusto.es>
2025-10-29 20:23:00 +0100

    Arreglo de mis commits

--- src/Swing/VentanaInstalaciones.java
+++ src/Swing/VentanaInstalaciones.java
@@ -10,6 +10,7 @@ public class VentanaInstalaciones extends JFrame{
 	private static final long serialVersionUID = 1L;
 	public VentanaInstalaciones() {
 		setBounds(400, 200, 800, 500);
+		setTitle("DeustoClubSports");
 		
 		
 		
